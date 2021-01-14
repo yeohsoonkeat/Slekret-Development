f4c8affe58f39224bd691557b9c755c31f2823cf
Chensokheng <chensokheng6@gmail.com>
2021-01-14 09:01:16 +0700

    :tada: Forum question editor

--- client/src/pages/forum/layout/ForumLayout.js
+++ client/src/pages/forum/layout/ForumLayout.js
@@ -6,31 +6,31 @@ import IconQuestion from '../../../icons/ic_question';
 import IconButton from '../components/IconButton';
 
 const ForumLayout = ({ children }) => {
-  const [activeIndex, setActiveIndex] = useState(0);
+	const [activeIndex, setActiveIndex] = useState(0);
 
-  return (
-    <div className="w-full max-w-8xl flex-1 px-2 sm:px-6 flex my-8">
-      <ul className="w-48 divide-y hidden lg:block mr-8">
-        {[
-          { title: 'Home', icon: IconHome },
-          { title: 'Explore Topics', icon: IconGlobe },
-          { title: 'My Questions', icon: IconQuestion },
-          { title: 'My Answers', icon: IconAnnotation },
-        ].map((category, index) => {
-          return (
-            <IconButton
-              key={index}
-              title={category.title}
-              icon={category.icon}
-              isActive={index === activeIndex}
-              onClick={() => setActiveIndex(index)}
-            />
-          );
-        })}
-      </ul>
-      {children}
-    </div>
-  );
+	return (
+		<div className="w-full max-w-8xl flex-1 px-2 sm:px-6 flex my-8">
+			<ul className="w-48 divide-y hidden lg:block mr-8">
+				{[
+					{ title: 'Home', icon: IconHome },
+					{ title: 'Explore Topics', icon: IconGlobe },
+					{ title: 'My Questions', icon: IconQuestion },
+					{ title: 'My Answers', icon: IconAnnotation },
+				].map((category, index) => {
+					return (
+						<IconButton
+							key={index}
+							title={category.title}
+							icon={category.icon}
+							isActive={index === activeIndex}
+							onClick={() => setActiveIndex(index)}
+						/>
+					);
+				})}
+			</ul>
+			<main className="w-full">{children}</main>
+		</div>
+	);
 };
 
 export default ForumLayout;
--- client/src/pages/forum/view/NewQuestion.js
+++ client/src/pages/forum/view/NewQuestion.js
@@ -1,17 +1,66 @@
 import { useState } from 'react';
-import ReactMarkdown from 'react-markdown';
+import IconEdit from '../../../icons/ic_edit';
+import IconPlus from '../../../icons/ic_plus';
+import IconPreview from '../../../icons/ic_preview';
+import QuestionEditor from '../components/QuestionEditor';
+import QuestionPreview from '../components/QuestionPreview';
 
 export default function NewQuestion() {
-	const [content, setContent] = useState('');
-	const handleEditorChange = (e) => {
-		console.log(e.target.value);
-		setContent(e.target.value);
-	};
+	const [questionTitle, setQuestionTitle] = useState('');
+	const [questionDescription, setQuestionDescription] = useState('');
+	const [listOfTags, setListofTags] = useState([]);
+	const [isHidden, setHidden] = useState(true);
+
+	const handleAskQuestion = () => {};
+
 	return (
-		<div
-			className="w-full ring-1 h-screen px-2"
-			contentEditable="true"
-			onChange={handleEditorChange}
-		></div>
+		<>
+			<div className="flex justify-between">
+				<button
+					className="px-4 py-2 bg-blue-600 text-sm text-white rounded-md flex items-center mb-2"
+					onClick={handleAskQuestion}
+				>
+					<IconPlus className="w-5 h-5" />
+					<span className="ml-1 font-medium ">Post Question</span>
+				</button>
+				<button
+					className="ml-2 px-4 py-2  ring-2  text-sm  rounded-md flex items-center mb-2 hover:bg-blue-500 hover:text-white hover:outline-none xl:hidden"
+					onClick={() => setHidden(!isHidden)}
+				>
+					{!isHidden ? (
+						<>
+							<IconEdit className="w-5 h-5" strokeWidth={2} />
+							<span className="ml-1 font-medium">Edit</span>
+						</>
+					) : (
+						<>
+							<IconPreview className="w-5 h-5" strokeWidth={2} />
+							<span className="ml-1 font-medium ">Preview</span>
+						</>
+					)}
+				</button>
+			</div>
+
+			<div className="h-screen w-full  bg-white flex ">
+				<QuestionEditor
+					className={`${
+						isHidden ? 'block' : 'hidden'
+					} xl:block xl:w-6/12 w-full   `}
+					setQuestionTitle={setQuestionTitle}
+					setQuestionDescription={setQuestionDescription}
+					setListofTags={setListofTags}
+					listOfTags={listOfTags}
+				/>
+
+				<QuestionPreview
+					className={`${
+						isHidden ? 'hidden' : 'block'
+					} xl:block xl:w-6/12 w-full  xl:max-w-4xl`}
+					questionTitle={questionTitle}
+					questionDescription={questionDescription}
+					listOfTags={listOfTags}
+				/>
+			</div>
+		</>
 	);
 }
