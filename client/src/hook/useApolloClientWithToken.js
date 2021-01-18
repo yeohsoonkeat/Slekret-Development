06a6c11ee7da0cb77e252d74875156c44dc7259a
Chensokheng <chensokheng6@gmail.com>
2021-01-18 10:08:26 +0700

    :bug: Fixed appollo

--- client/src/hook/useApolloClientWithToken.js
+++ client/src/hook/useApolloClientWithToken.js
@@ -1,64 +1,59 @@
 import {
-  ApolloClient,
-  HttpLink,
-  InMemoryCache,
+	ApolloClient,
+	ApolloLink,
+	HttpLink,
+	concat,
+	InMemoryCache,
 } from '@apollo/client';
 import { onError } from 'apollo-link-error';
 import axios from 'axios';
-import config from '../config';
 
 export default function useApolloClientWithToken(token, authDispatch) {
-  const httpLink = new HttpLink({
-    uri: config.hasuraGraphql,
-    headers: {
-      Authorization: `Bearer ${token ? token : config.guestToken}`
-    },
-  });
+	const httpLink = new HttpLink({
+		uri: 'http://localhost:8080/v1/graphql',
+	});
 
-  const logoutLink = onError(({ graphQLErrors, networkError, forward }) => {
-    if (graphQLErrors) {
-      graphQLErrors.forEach(({ extensions }) => {
-        console.log(extensions)
-        if (extensions.code === 'invalid-jwt') {
-          axios
-            .post(
-              config.backendUrl + '/auth/logout',
-              {},
-              {
-                withCredentials: true,
-                headers: {
-                  Accept: 'application/json',
-                  'Content-Type': 'application/json',
-                  'Access-Control-Allow-Credentials': true,
-                },
-              }
-            )
-            .then(() => {
-              window.open('/', '_self');
-            });
-          authDispatch({
-            type: 'USER_LOGOUT',
-          });
-          window.localStorage.setItem('auth', 'false');
-        }
-      });
-    }
-    if (networkError && networkError.statusCode === 401)
-      console.log('hello world');
-  });
+	const logoutLink = onError(({ graphQLErrors, networkError, forward }) => {
+		if (graphQLErrors) {
+			graphQLErrors.forEach(({ extensions }) => {
+				if (extensions.code === 'invalid-jwt') {
+					axios
+						.get('http://localhost:8000/logout', {
+							withCredentials: true,
+							headers: {
+								Accept: 'application/json',
+								'Content-Type': 'application/json',
+								'Access-Control-Allow-Credentials': true,
+							},
+						})
+						.then(() => {
+							window.open('/authentication', '_self');
+						});
+					authDispatch({
+						type: 'USER_LOGOUT',
+					});
+					window.localStorage.setItem('auth', 'false');
+				}
+			});
+		}
+		if (networkError && networkError.statusCode === 401)
+			console.log('hello world');
+	});
 
-  // const authMiddleware = new ApolloLink((operation, forward) => {
-  //   if (token) {
-  //     operation.setContext({
-  
-  //     });
-  //   }
-  //   return forward(operation);
-  // });
+	const authMiddleware = new ApolloLink((operation, forward) => {
+		if (token) {
+			operation.setContext({
+				headers: {
+					Authorization: `Bearer ${token}`,
+				},
+			});
+		}
+		return forward(operation);
+	});
 
-  const apolloClient = new ApolloClient({
-    link:logoutLink.concat(httpLink),
-    cache: new InMemoryCache(),
-  });
-  return { apolloClient };
+	const apolloClient = new ApolloClient({
+		link: logoutLink.concat(concat(authMiddleware, httpLink)),
+		cache: new InMemoryCache(),
+	});
+	return { apolloClient };
 }
