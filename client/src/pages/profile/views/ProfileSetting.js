58884f1766f5419f4596b6dc4c2b645d12009d7f
Chensokheng <chensokheng6@gmail.com>
2021-02-10 02:21:12 +0700

    :tada: Finish updating tags

--- client/src/pages/profile/views/ProfileSetting.js
+++ client/src/pages/profile/views/ProfileSetting.js
@@ -2,8 +2,13 @@ import React from 'react';
 
 export default function ProfileSetting() {
 	return (
-		<div>
-			<h1>Hello world</h1>
+		<div className="max-w-5xl mx-auto bg-blue-500 min-h-screen flex">
+			<div>
+				<ul>
+					<li>Personal Information</li>
+				</ul>
+			</div>
+			<div></div>
 		</div>
 	);
 }
